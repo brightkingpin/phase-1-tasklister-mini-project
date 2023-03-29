f43131b17d63c59af98291318cf66e0b0b22e1e2
brightkingpin <luvandabright781@gmail.com>
2023-03-29 12:42:03 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // This is my TaskLister Mini-Project
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // prevent default form submission behavior
+
+    // create a new task list item
+
+    const taskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = taskDescription.value;
+
+    // add the new task to the task list
+    taskList.appendChild(newTask);
+
+    // reset the form input
+    taskDescription.value = "";
+  });
 });
